4308f57cd841ef4c87a092f96022ad1986d443e6
Midori <88923030+Midodo22@users.noreply.github.com>
2025-05-30 20:39:08 +0800

    Initial CNN model

---
+++ code/CNN.py
@@ -0,0 +1,214 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from typing import Tuple
+import pandas as pd
+
+class CNN(nn.Module):
+    def __init__(self, num_classes = 62):
+        # (TODO) Design your CNN, it can only be less than 3 convolution layers
+        super(CNN, self).__init__()
+        self.layer1 = nn.Conv2d(in_channels = 3, out_channels = 32, kernel_size = 3)
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU()
+        self.max_pool1 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        self.layer2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU()
+        self.max_pool2 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        self.layer3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3)
+        self.batchnorm3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
+        self.max_pool3 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        self.layer4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3)
+        self.batchnorm4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU()
+        self.max_pool4 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        self.layer5 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 3)
+        self.batchnorm5 = nn.BatchNorm2d(32)
+        self.relu5 = nn.ReLU()
+        self.max_pool5 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        self.layer6 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
+        self.batchnorm6 = nn.BatchNorm2d(32)
+        self.relu6 = nn.ReLU()
+        self.max_pool6 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        
+        # compute the flattened size dynamically
+        dummy_input = torch.zeros(1, 3, 224, 224)  # adjust to input image size
+        x = self.max_pool1(self.relu1(self.layer1(dummy_input)))
+        x = self.max_pool2(self.relu2(self.layer2(x)))
+        x = self.max_pool3(self.relu3(self.layer3(x)))
+        x = self.max_pool4(self.relu4(self.layer4(x)))
+        x = self.max_pool5(self.relu5(self.layer5(x)))
+        x = self.max_pool6(self.relu6(self.layer6(x)))
+        flattened_size = x.view(1, -1).shape[1]  # Get the flattened size
+        
+        self.fc1 = nn.Linear(flattened_size, 128)
+        self.relu_last = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, num_classes)
+        self.logSoftmax = nn.LogSoftmax(dim = 1)
+
+    def forward(self, x):
+        # (TODO) Forward the model
+        # pass through first set
+        x = self.layer1(x)
+        x = self.batchnorm1(x)
+        x = self.relu1(x)
+        x = self.max_pool1(x)
+        
+        # pass through second set
+        x = self.layer2(x)
+        x = self.batchnorm2(x)
+        x = self.relu2(x)
+        x = self.max_pool2(x)
+        
+        # pass through third set
+        x = self.layer3(x)
+        x = self.batchnorm3(x)
+        x = self.relu3(x)
+        x = self.max_pool3(x)
+        
+        # pass through fourth set
+        x = self.layer4(x)
+        x = self.batchnorm4(x)
+        x = self.relu4(x)
+        x = self.max_pool4(x)
+        
+        # pass through fifth set
+        x = self.layer5(x)
+        x = self.batchnorm5(x)
+        x = self.relu5(x)
+        x = self.max_pool5(x)
+        
+        # pass through sixth set
+        x = self.layer6(x)
+        x = self.batchnorm6(x)
+        x = self.relu6(x)
+        x = self.max_pool6(x)
+        
+        # flatten output and pass through fc -> relu layers
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        
+        self.dropout = nn.Dropout(0.3)
+        
+        # pass to softmax classfier
+        x = self.fc2(x)
+        x = self.relu_last(x)
+        x = self.logSoftmax(x)
+        
+        return x
+
+def train(model: CNN, train_loader: DataLoader, criterion, optimizer, device)->float:
+    # (TODO) Train the model and return the average loss of the data, we suggest use tqdm to know the progress
+    model.train()
+    total_loss = 0.0
+    steps = len(train_loader) # batch size
+    
+    model.to(device)
+    
+    # tqdm progress bar
+    loop = tqdm(train_loader, desc = "Training", leave = False)
+    
+    for inputs, labels in loop:
+        # move data to correct device
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        # forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        
+        # zero the gradients, backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        # update model parameters
+        optimizer.step()
+        
+        # update running loss
+        total_loss += loss.item()
+        
+        # update tqdm description with current loss
+        loop.set_postfix(loss = total_loss)
+        
+    avg_loss = total_loss / steps    
+    # accuracy = correct / steps
+    
+    return avg_loss
+
+
+def validate(model: CNN, val_loader: DataLoader, criterion, device)->Tuple[float, float]:
+    # (TODO) Validate the model and return the average loss and accuracy of the data, we suggest use tqdm to know the progress
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    # set model to evaluation mode
+    model.eval()
+    model.to(device)
+    
+    with torch.no_grad():
+        loop = tqdm(val_loader, desc = "Validating", leave = False)
+        
+        for inputs, labels in loop:
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            # get output and calculate loss
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            
+            total_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+            loop.set_postfix(loss = total_loss)
+    
+    # this feels wrong
+    avg_loss = total_loss / len(val_loader)
+    accuracy = correct / total * 100
+    
+    return avg_loss, accuracy
+
+def test(model: CNN, test_loader: DataLoader, criterion, device):
+    # (TODO) Test the model on testing dataset and write the result to 'CNN.csv'
+    # set model to evaluation mode
+    model.eval()
+    model.to(device)
+    predictions = []
+    name = []
+    
+    with torch.no_grad():
+        loop = tqdm(test_loader, desc = "Validating", leave = False)
+        
+        for inputs, nm in loop:
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+            
+            img, predicted = torch.max(outputs.data, 1)
+            predictions.extend(predicted.cpu().numpy())
+            name.extend(nm)
+            
+    # Write predictions to CSV
+    pred = []
+    for idx, lb in enumerate(predictions):
+        # id.append(lbl)
+        pred.append(lb)
+        
+    data = {
+        "id" : name,
+        "prediction" : pred
+    }
+    
+    df = pd.DataFrame(data)
+    df.to_csv("CNN.csv", index = False)
+            
+    print(f"Predictions saved to 'CNN.csv'")
+    return
---
+++ code/utils.py
@@ -0,0 +1,101 @@
+from torchvision import transforms
+from torch.utils.data import Dataset
+import os
+import PIL
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+
+class TrainDataset(Dataset):
+    def __init__(self, images, labels):
+        self.transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
+        self.images, self.labels = images, labels
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_path = self.images[idx]
+        image = PIL.Image.open(image_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = self.labels[idx]
+        return image, label
+
+class TestDataset(Dataset):
+    def __init__(self, image):
+        self.transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
+        self.image = image
+
+    def __len__(self):
+        return len(self.image)
+
+    def __getitem__(self, idx):
+        image_path = self.image[idx]
+        image = PIL.Image.open(image_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        base_name = os.path.splitext(os.path.basename(image_path))[0]
+        return image, base_name
+    
+def load_train_dataset(path: str='train_data/')->Tuple[List, List]:
+    images = []
+    labels = []
+    
+    folders = os.listdir(path)
+    for f in folders:
+        label = int(f)
+        image_path = path + f + '/'
+        
+        pics = os.listdir(image_path)
+        for pic in pics:
+            images.append(image_path + "/" + pic)
+            labels.append(label)
+    
+    return images, labels
+
+def load_test_dataset(path: str='test_data/')->List:
+    images = []
+    pics = os.listdir(path)
+    for pic in pics:
+        images.append(path + pic)
+    return images
+
+def name_file(base_name, ext=".png"):
+    i = 1.0
+    filename = f"{base_name}{i}{ext}"
+    while os.path.exists(filename):
+        i += 0.1
+        i = round(i, 2)
+        filename = f"{base_name}{i}{ext}"
+    return filename
+
+def plot(train_losses: List, val_losses: List):
+    # (TODO) Plot the training loss and validation loss of CNN, and save the plot to 'loss.png'
+    #        xlabel: 'Epoch', ylabel: 'Loss'
+    
+    epoch = range(len(train_losses))
+    plt.plot(epoch, train_losses, 's-c', label = 'Training Loss')
+    plt.plot(epoch, val_losses, 'o-g', label='Val Losses')
+    
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training Loss and Validation Loss of CNN')
+    plt.grid(True)
+    
+    plt.legend()
+    filename = name_file("plot_images/loss", ".png")
+    plt.savefig(filename)
+    print(f"Saved the plot to '{filename}'")
+    return
