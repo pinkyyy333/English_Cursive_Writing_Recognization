51ec7b5b39092a3962850121ef4aba15651976b7
SlowDown8270 <105618659+mchien728@users.noreply.github.com>
2025-06-01 18:47:27 +0800

    Update BiLSTM_CTC

--- CNN_BiLSTM_CTC.py
+++ CNN_BiLSTM_CTC.py
@@ -6,28 +6,46 @@ class CNN_BiLSTM_CTC(nn.Module):
         super(CNN_BiLSTM_CTC, self).__init__()
 
         self.cnn = cnn_backbone
-        self._feature_size = None  # 等待 forward 初始化
+        # self._feature_size = None  # 等待 forward 初始化
 
         self.lstm_hidden = lstm_hidden
         self.lstm_layers = lstm_layers
         self.num_classes = num_classes
+
+        dummy_input = torch.zeros(1, 1, 128, 512)  # 根據你的圖片大小設定
+        with torch.no_grad():
+            features = self.cnn.extract_features(dummy_input)  # [1, C, H, W]
+            print(features.shape)  # b, c, h, w
+            print("Output time steps (width):", features.shape[-1])
+            print("[DEBUG] features shape from dummy input:", features.shape)
+            b, c, h, w = features.shape
+            self.seq_len = w
+            self.feature_size = c * h  # 每個時間步的特徵維度
+
+        self.lstm = nn.LSTM(
+                input_size=self.feature_size,
+                hidden_size=self.lstm_hidden,
+                num_layers=self.lstm_layers,
+                batch_first=True,
+                bidirectional=True
+            )
+        self.classifier = nn.Linear(self.lstm_hidden * 2, self.num_classes)
+        self._init_weights()
+
+    def _init_weights(self):
+        for name, param in self.lstm.named_parameters():
+            if 'weight' in name:
+                nn.init.xavier_uniform_(param)
+            elif 'bias' in name:
+                nn.init.constant_(param, 0)
+        nn.init.xavier_uniform_(self.classifier.weight)
+        nn.init.constant_(self.classifier.bias, 0)
 
     def forward(self, x):
         features = self.cnn.extract_features(x)  # [B, C, H, W]
         b, c, h, w = features.size()
         features = features.permute(0, 3, 1, 2).contiguous()  # [B, W, C, H]
         features = features.view(b, w, -1)  # [B, W, C*H]
-
-        if self._feature_size is None:
-            self._feature_size = features.shape[2]
-            self.lstm = nn.LSTM(
-                input_size=self._feature_size,
-                hidden_size=self.lstm_hidden,
-                num_layers=self.lstm_layers,
-                batch_first=True,
-                bidirectional=True
-            )
-            self.classifier = nn.Linear(self.lstm_hidden * 2, self.num_classes)
 
         lstm_out, _ = self.lstm(features)
         out = self.classifier(lstm_out)
